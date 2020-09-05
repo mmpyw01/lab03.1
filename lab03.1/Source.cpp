0d3ae45f13484324e92c9cc95498b4dbfb7f0274
PIYAWAN NORPALM <63010608@KMITL.AC.TH>
2020-09-05 22:13:08 +0700

    Add project files.

---
+++ lab03.1/Source.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+	int num, a, b, i = 0, k, sum, sum2 = 0;
+	int array_a[100], array_b[100];
+
+	printf("Enter number : ");
+	scanf("%d", &num);
+	num = num * 10;
+
+
+	while (num != 0)
+	{
+		a = num % 10;
+		array_a[i] = a;
+		num = num / 10;
+		i++;
+	}
+
+	for (int j = i - 1; j > 0; j--)
+	{
+		sum = (sum + array_a[j]);
+	}
+	printf("(%d)", sum);
+
+	while (sum != 0)
+	{
+		b = sum % 10;
+		array_b[k] = b;
+		sum = sum / 10;
+		k++;
+	}
+
+	for (int m = k - 1; m > 0; m--)
+	{
+		sum2 = (sum2 + array_b[m]);
+	}
+	printf("\n(%d)", sum2);
+}
